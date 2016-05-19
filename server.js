e132eeb9bd035c064d1a8a67fffb517e708def08
Jonas Almeida <jalmeida@mathbiol.org>
2016-05-19 10:22:50 -0400

    venus added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var t0 = new Date();
-var port = process.env.PORT||3000
+var port = process.env.PORT||3000;
 
 app.get('/', function (req, res) {
     res.send('Hello World! from server started at '+t0);
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
 
 app.get('/mars', function (req, res) {
     res.send('Hello from Mars at '+new Date()+' ! from server started at '+t0);
+});
+
+app.get('/venus', function (req, res) {
+    res.send('Hello from venus at '+new Date()+' ! from server started at '+t0);
 });
 
 app.listen(port, function () {
