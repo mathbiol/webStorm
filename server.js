d8047b103db77ce62384ec6e8333548696537f66
Jonas Almeida <jalmeida@mathbiol.org>
2016-05-19 12:42:19 -0400

    add redcap

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express = require('express');
+var express = require('express'); // for listening
 var app = express();
 var t0 = new Date();
 var port = process.env.PORT||3000;
+var request = require("request");  //for calling
 
 app.get('/', function (req, res) {
     res.send('Hello World! from server started at '+t0);
@@ -23,6 +24,75 @@ app.get('/jupiter', function (req, res) {
     res.send('Hello from Jupiter at '+new Date()+' ! from server started at '+t0);
 });
 
-app.listen(port, function () {
-    console.log('Example app listening on port '+port);
+app.get('/redcap', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*"); // CORS
+    // can you connect to redcap?
+    var options = { method: 'POST',
+        url: 'https://redcap.stonybrookmedicine.edu/redcap/api/',
+        headers:
+        { 'content-type': 'application/x-www-form-urlencoded',
+            'cache-control': 'no-cache' },
+        form:
+        { content: 'metadata',
+            token: process.env.redcapAPIkey,
+            format: 'json' } };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        //console.log(body);
+        res.send(body)
+
+    });
+    
+    //res.send('Hello from Jupiter at '+new Date()+' ! from server started at '+t0);
 });
+
+app.listen(port, function () {
+    console.log('listening on port '+port+' since '+t0);
+});
+
+
+
+/*
+
+--- server ---
+
+ var request = require("request");
+
+ var options = { method: 'POST',
+ url: 'https://redcap.stonybrookmedicine.edu/redcap/api/',
+ headers:
+ { 'content-type': 'application/x-www-form-urlencoded',
+ 'cache-control': 'no-cache' },
+ form:
+ { content: 'metadata',
+ token: process.env.redcapAPIkey,
+ format: 'json' } };
+
+ request(options, function (error, response, body) {
+ if (error) throw new Error(error);
+
+ console.log(body);
+ });
+
+--- client ---
+
+ var data = "content=metadata&token="+redcapAPIkey+"&format=json";
+
+ var xhr = new XMLHttpRequest();
+ xhr.withCredentials = true;
+
+ xhr.addEventListener("readystatechange", function () {
+ if (this.readyState === 4) {
+ console.log(this.responseText);
+ }
+ });
+
+ xhr.open("POST", "https://redcap.stonybrookmedicine.edu/redcap/api/");
+ xhr.setRequestHeader("cache-control", "no-cache");
+ xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+
+ xhr.send(data);
+
+ */
